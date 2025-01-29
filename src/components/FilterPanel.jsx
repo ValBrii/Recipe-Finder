78478c779039b9f50144ccd8cd6db6078ad6ec92
+ <+>
2025-01-29 14:50:00 +0300

    updated filter panel

--- src/components/FilterPanel.jsx
+++ src/components/FilterPanel.jsx
@@ -2,5 +2,9 @@ import react from 'react';
 import { useState, useEffect } from 'react';
 
 function FilterPanel(){
+    const [filter, setFilter] = useState('');
     
+
+
 }
+export default FilterPanel
