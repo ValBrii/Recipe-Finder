b1f0daf735d9f6083d25e0f733f95498912b42f3
Valerie <britneyvalerie85@gmail.com>
2025-01-29 19:05:14 +0300

    changes made

--- src/components/RecipeData.jsx
+++
@@ -1,7 +0,0 @@
-const [recipes, setRecipes] = useState([]);
-
-useEffect(() => {
-  fetch('')
-    .then(response => response.json())
-    .then(data => setRecipes(data));
-}, []);
---
+++ src/components/RecipeDetail.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+
+
+function RecipeDetail() {
+ 
+  const [recipe, setRecipe] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/recipes/${id}`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch recipe");
+        }
+        return response.json();
+      })
+      .then((data) => setRecipe(data))
+      .catch((error) => setError(error.message));
+  }, [id]);
+
+  if (error) return <p>Error: {error}</p>;
+  if (!recipe) return <p>Loading recipe...</p>;
+
+  return (
+    <div>
+      <h1>{recipe.title}</h1>
+      <img src={recipe.image} alt={recipe.title}  />
+      <h3>Ingredients:</h3>
+      <ul>
+        {recipe.ingredients.map((ingredient, index) => (
+          <li key={index}>{ingredient}</li>
+        ))}
+      </ul>
+      <p>Diet Type: {recipe.diet}</p>
+    </div>
+  );
+}
+
+export default RecipeDetail;
