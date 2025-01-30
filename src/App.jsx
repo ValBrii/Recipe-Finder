83bf0122ca529207b175a75c55c316927ba16873
FlozyTess <cecilrency23@gmail.com>
2025-01-30 14:24:45 +0300

    imported all the components

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import SearchForm from './components/SearchForm';
+import RecipeList from './components/RecipeList';
+import RecipeDetail from './components/RecipeDetail';
+import FilterPanel from './components/FilterPanel';
+import Footer from './components/Footer';
