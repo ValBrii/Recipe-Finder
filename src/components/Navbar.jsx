bf4156a9cc0674411fa91e670d14655935edac84
FlozyTess <cecilrency23@gmail.com>
2025-01-30 04:44:54 +0300

    added nav links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 const Navbar = () => {
-    return 
-    }
+    return (
+        <nav className="navbar">
+          <div className="navbar-container">
+            <h1 className="navbar-logo">Recipe Finder</h1>
+            <ul className="navbar-links">
+                <li className="navbar-item">
+            <Link to="/" className="navbar-link">Home</Link>
+          </li>
+          </ul>
+            </div>
+            </nav>
+            )
+    };
