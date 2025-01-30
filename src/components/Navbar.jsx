8a8ed69721f8dbf56545bb60b5bbb06921e56b7a
FlozyTess <cecilrency23@gmail.com>
2025-01-30 04:59:09 +0300

    created nav links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,28 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 const Navbar = () => {
     return (
-        <nav className="navbar">
-          <div className="navbar-container">
-            <h1 className="navbar-logo">Recipe Finder</h1>
-            <ul className="navbar-links">
-                <li className="navbar-item">
-            <Link to="/" className="navbar-link">Home</Link>
-          </li>
-          </ul>
-            </div>
-            </nav>
-            )
-    };
+      <nav className="navbar">
+           <div className="navbar-container">
+                <h1 className="navbar-logo">Recipe Finder</h1>
+                <ul className="navbar-links">
+                       <li className="navbar-item">
+                           <Link to="/" className="navbar-link">Home</Link>
+                       </li>
+                       <li className="navbar-item">
+                           <Link to="/" className="navbar-link">Recipe list</Link>
+                       </li>
+                       <li className="navbar-item">
+                           <Link to="/" className="navbar-link">Recipe</Link>
+                       </li>
+                       <li className="navbar-item">
+                           <Link to="/" className="navbar-link">Search</Link>
+                       </li>
+                       <li className="navbar-item">
+                           <Link to="/" className="navbar-link">Filter</Link>
+                       </li>
+                </ul>
+         </div>
+       </nav>
+    );
+ };
+ export default Navbar;
