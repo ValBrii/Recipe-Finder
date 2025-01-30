c60272eef28a5fe035360fb663d2eb58862b2a8f
FlozyTess <cecilrency23@gmail.com>
2025-01-30 14:39:05 +0300

    fetch recipe data

--- src/App.jsx
+++ src/App.jsx
@@ -6,3 +6,18 @@ import RecipeList from './components/RecipeList';
 import RecipeDetail from './src/components/RecipeDetail';
 import FilterPanel from './src/components/FilterPanel';
 import Footer from './src/components/Footer';
+const App = () => {
+  const [recipes, setRecipes] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filter, setFilter] = useState('');
+  const [loading, setLoading] = useState(false);
+  const fetchRecipes = () => {
+    setLoading(true);
+    let url = `http://localhost:3001/recipes?ingredient=${searchTerm}&diet=${filter}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setRecipes(data);
+        setLoading(false);
+      })};
+};
