ee922cbf20a19350b59d5402009087df29fe960b
Visionnaire1000 <cheruiyotlouis@gmail.com>
2025-01-31 04:48:06 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer className="footer">
-            <p>&copy; 2025 Recipe Finder App. All rights reserved.</p>
+            <p id="copy">&copy; 2025 Recipe Finder App. All rights reserved.</p>
         </footer>
     );
     };
