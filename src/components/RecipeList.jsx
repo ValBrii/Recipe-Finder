5244573fea8c9287591c94d15df1c21b70dd309c
Visionnaire1000 <cheruiyotlouis@gmail.com>
2025-01-31 04:53:44 +0300

    Update RecipeList.jsx

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -1,26 +1,28 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-function RecipeList({ recipes }) {
-  if (recipes.length === 0) {
-    return <p>No Recipes Found</p>;
-  }
-
+function RecipeList({ recipes, searchPerformed, loading }) {
   return (
-    <ul>
-      {recipes.map((recipe) => (
-        <li key={recipe.id}>
-          <Link to={`/recipe/${recipe.id}`}>
-            <h3>{recipe.title}</h3>
-            <img src={recipe.image} alt={recipe.title}  />
-          </Link>
-        </li>
-      ))}
-    </ul>
+    <div className="recipe-list-container">
+      {/* Show "Loading..." while fetching */}
+      {loading ? (
+        <p>Loading...</p>
+      ) : searchPerformed && recipes.length === 0 ? (
+        <p id="alert">Recipe Not Found</p>
+      ) : (
+        <div className="recipe-list">
+          {recipes.map((recipe) => (
+            <Link key={recipe.id} to={`/recipe/${recipe.id}`}>
+              <div className="recipe-card">
+                <img src={recipe.image} alt={recipe.title} />
+                <p>{recipe.title}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
+      )}
+    </div>
   );
 }
 
-export default RecipeList
-
-
-
+export default RecipeList;
