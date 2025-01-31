65bab08df3adfedc4e648c5c36a4e814a551e22d
Visionnaire1000 <cheruiyotlouis@gmail.com>
2025-01-31 04:48:36 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
     return (
       <nav className="navbar">
-           <div className="navbar-container">
-                <h1 className="navbar-logo">Recipe Finder</h1>
-                <ul className="navbar-links">
-                       <li className="navbar-item">
-                           <Link to="/" className="navbar-link">Home</Link>
-                       </li>
-                       <li className="navbar-item">
-                           <Link to="/Search" className="navbar-link">Recipe list</Link>
-                       </li>
-                       <li className="navbar-item">
-                           <Link to="recipe/:id" className="navbar-link">Recipe</Link>
-                       </li>
-                       <li className="navbar-item">
-                           <Link to="/Search" className="navbar-link">Search</Link>
-                       </li>
-                </ul>
-         </div>
-       </nav>
+          <Link to="/">HOME</Link>
+          <Link to="/search">SEARCH MEAL</Link>
+          <Link to="/filter" className="navbar-link">SELECT DIET</Link>
+      </nav>
     );
- };
- export default Navbar;
+};
+
+export default Navbar;
