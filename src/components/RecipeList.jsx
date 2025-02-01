9d9fc79c0574e78d2da0665a09fa07f99b564c57
Visionnaire1000 <cheruiyotlouis@gmail.com>
2025-02-01 03:15:24 +0300

    Update RecipeList.jsx

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -3,25 +3,23 @@ import { Link } from 'react-router-dom';
 
 function RecipeList({ recipes, searchPerformed, loading }) {
   return (
-    <div className="recipe-list-container">
-      {/* Show "Loading..." while fetching */}
+    <>
       {loading ? (
-        <p>Loading...</p>
+        <p className='loading'>Loading...</p>
       ) : searchPerformed && recipes.length === 0 ? (
-        <p id="alert">Recipe Not Found</p>
+        <p id="alert">Meal not in our database!!</p>
       ) : (
         <div className="recipe-list">
           {recipes.map((recipe) => (
             <Link key={recipe.id} to={`/recipe/${recipe.id}`}>
-              <div className="recipe-card">
-                <img src={recipe.image} alt={recipe.title} />
-                <p>{recipe.title}</p>
-              </div>
+              <p>Click image below to view recipe</p>
+              <img src={recipe.image} alt={recipe.title} />
+              <p>{recipe.title}</p>
             </Link>
           ))}
         </div>
       )}
-    </div>
+    </>
   );
 }
 
