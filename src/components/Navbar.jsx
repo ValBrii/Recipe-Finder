366789c197dba368a66839df2d9ac982b9e26237
Visionnaire1000 <cheruiyotlouis@gmail.com>
2025-02-01 03:13:37 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
     return (
       <nav className="navbar">
-          <Link to="/">HOME</Link>
           <Link to="/search">SEARCH MEAL</Link>
           <Link to="/filter" className="navbar-link">SELECT DIET</Link>
       </nav>
